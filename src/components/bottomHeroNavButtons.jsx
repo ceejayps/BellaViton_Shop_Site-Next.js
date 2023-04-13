3d9813debc4988e1e59fa6c83c0fcecb7af0c680
ceejayps <ceejayps1308@gmail.com>
2023-04-13 06:24:23 -0500

    Create bottomHeroNavButtons.jsx

---
+++ src/components/bottomHeroNavButtons.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+
+const BottomHeroNavButtons = () => {
+  return (
+    <div className="flex h-12 w-64 divide-x overflow-hidden rounded-lg border">
+    <Link href="#"
+        className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Men</Link> 
+    <Link href="#"
+        className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Women</Link> 
+    <Link href="#"
+        className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Teens</Link> 
+</div>
+  )
+}
+
+export default BottomHeroNavButtons
