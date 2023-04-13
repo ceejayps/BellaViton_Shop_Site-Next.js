a85262bc76bf1628085168bd8ff778f94ffd58b3
ceejayps <ceejayps1308@gmail.com>
2023-04-13 09:53:17 -0500

    Update callectionCard.jsx

--- src/components/callectionCard.jsx
+++ src/components/callectionCard.jsx
@@ -4,7 +4,7 @@ const CollectionCard = (props) => {
   const { title, captionType, altText, image } = props;
   return (
     <div>
-      <Link href="#" className="group mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
+      <Link href={`/products/Collections/${captionType}`} className="group mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
         <img
           src={image}
           loading="lazy"
@@ -15,7 +15,7 @@ const CollectionCard = (props) => {
       <div className="flex flex-col">
         <span className="text-gray-500">{captionType}</span>
         <Link
-          href="#"
+          href={`/products/Collections/${captionType}`}
           className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl"
         >
           {title}
