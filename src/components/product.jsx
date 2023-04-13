d863c8edf4ebc5979a59eec58d3cdaa03632aaef
ceejayps <ceejayps1308@gmail.com>
2023-04-13 09:53:14 -0500

    Update product.jsx

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -6,7 +6,7 @@ function Product(props) {
   
   return (
     <div>
-      <Link href={`/productdetail/?productid=1`} className="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
+      <Link href={`/productdetail/?productid=${id}`} className="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
         <img src={image} loading="lazy" alt={title} className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
         {salePrice && <span className="absolute left-0 top-0 rounded-br-lg bg-red-500 px-3 py-1.5 text-sm uppercase tracking-wider text-white">sale</span>}
       </Link>
