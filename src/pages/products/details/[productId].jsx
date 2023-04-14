2ac14231b5e5e067a5e98873b7d3636d8fab92a4
ceejayps <ceejayps1308@gmail.com>
2023-04-14 03:53:21 -0500

    Update [productId].jsx

--- src/pages/products/details/[productId].jsx
+++ src/pages/products/details/[productId].jsx
@@ -3,13 +3,17 @@ import ProductDetails from '../../../components/products/productDetails'
 import Footer from '../../../components/footer/footer'
 import Nav from '../../../components/nav'
 import Header from '../../../components/header'
+import Breadcrum from '../../../components/products/breadcrum'
+
 
 const Details = () => {
   return (
     <>
-  <div className='bg-white pb-6 sm:pb-8 lg:pb-12 mx-auto max-w-screen-2xl px-4 md:px-8'>
+  <div className='bg-white pb-0 sm:pb-0 lg:pb-0 mx-auto max-w-screen-2xl px-4 md:px-8'>
         <Header />
         </div>
+        <Breadcrum/>
+        
     <ProductDetails/>
     <Footer/>
     </>
