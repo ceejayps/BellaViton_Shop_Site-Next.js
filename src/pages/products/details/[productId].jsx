152bdc2e1704392576ddc9148ef42a054c0c358e
Chester Johnson <59537158+ceejayps@users.noreply.github.com>
2023-12-20 11:23:37 -0500

    Update [productId].jsx

--- src/pages/products/details/[productId].jsx
+++ src/pages/products/details/[productId].jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import ProductDetails from '../../../components/products/productDetails'
-import Footer from '../../../components/footer/footer'
-import Nav from '../../../components/nav'
-import Header from '../../../components/header'
-import Breadcrum from '../../../components/products/breadcrum'
-
+import React from "react";
+import ProductDetails from "../../../components/products/productDetails";
+import Footer from "../../../components/footer/footer";
+import Nav from "../../../components/nav";
+import Header from "../../../components/header";
+import Breadcrum from "../../../components/products/breadcrum";
 
 const Details = () => {
   return (
     <>
-  <div className='bg-white pb-0 sm:pb-0 lg:pb-0 mx-auto max-w-screen-2xl px-4 md:px-8'>
+      <div className="bg-white pb-0 sm:pb-0 lg:pb-0 mx-auto max-w-screen-2xl px-4 md:px-8">
         <Header />
-        </div>
-        <Breadcrum/>
-        
-    <ProductDetails/>
-    <Footer/>
-    </>
-  )
-}
+      </div>
+      {/* <Breadcrum/> */}
 
-export default Details
+      <ProductDetails />
+      <Footer />
+    </>
+  );
+};
+
+export default Details;
